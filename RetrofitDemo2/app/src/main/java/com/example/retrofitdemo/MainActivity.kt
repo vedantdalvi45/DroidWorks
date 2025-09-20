654d0e4beaa4945dc67d3aecfbbe6a99c44fb092
vedantdalvi45 <vedalvi16@gmail.com>
2025-09-20 12:51:10 +0530

    init

--- RetrofitDemo2/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ RetrofitDemo2/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
